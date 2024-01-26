920d0317913710305e5913dd126a429cc21438a7
Conner4348 <92555576+Conner4348@users.noreply.github.com>
2024-01-25 19:39:04 -0500

    Added teams dictionary

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # Here is the main file that simulates the sports matches.
+
+
+
+# TEST DATA SET
+
+# Teams
+teams = {
+    'Argentina': 0,
+    'Brazil': 0,
+    'Uruguay': 0,
+    'Colombia': 0,
+    'Ecuador': 0,
+    'Peru': 0,
+    'Chile': 0,
+    'Venezuela': 0,
+    'Paraguay': 0,
+    'Bolivia': 0
+}
+
